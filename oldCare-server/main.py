c9b844cb99713655ddcf56a550ab4534aa240789
乔赛文 <1152716872@qq.com>
2022-07-04 16:55:07 +0800

    后端环境配置好了

--- oldCare-server/main.py
+++ oldCare-server/main.py
@@ -1,4 +1,8 @@
 # 这是一个示例 Python 脚本。
+from flask import Flask, render_template, Response, request, session
+from flask_cors import *
+from flask_sqlalchemy import SQLAlchemy
+from imutils import paths
 
 # 按 Shift+F10 执行或将其替换为您的代码。
 # 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
