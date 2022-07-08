9849cf162d8f719fe05ca3885d460bdb4f0bc2c7
乔赛文 <1152716872@qq.com>
2022-07-08 21:49:16 +0800

    1. 添加了获取首页相关数据接口、获取情感检测数据接口、获取陌生人及区域入侵检测数据接口、获取老人摔倒检测数据接口、获取老人与护工交互有检测数据接口 2. 对部分接口进行了修缮 3. 做测试时对test.py文件进行了修改

--- oldCare-server/app.py
+++ oldCare-server/app.py
@@ -1,13 +1,13 @@
-#-*-coding:UTF-8 -*-
+# -*-coding:UTF-8 -*-
 from flask import Flask, render_template, request, jsonify, session, Response
 from flask_sqlalchemy import SQLAlchemy
 from flask_siwadoc import SiwaDoc
 from pydantic import BaseModel, Field
+
 import base64
 import time
 import json
 import os
-
 
 from response import *
 from models.models import *
@@ -23,10 +23,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 # 查询时会显示原始SQL语句
 app.config['SQLALCHEMY_ECHO'] = True
 
-
 db = SQLAlchemy(app)
 
-#将Flask实例传入SiwaDoc的构造方法初始化SiwaDoc
+# 将Flask实例传入SiwaDoc的构造方法初始化SiwaDoc
 siwa = SiwaDoc(app)
 
 
@@ -38,21 +37,94 @@ def root():
     """
     return render_template('Index.html')
 
+
+@app.route("/homePage_data", methods=['Get'])
+def get_homePage_Data():
+    sql_number_oldPeople = "SELECT COUNT(*) FROM oldperson_info"
+    sql_number_gender = "SELECT COUNT(case when oldperson_info.gender='male' then oldperson_info.gender end) as 'male', " \
+                        "COUNT(case when oldperson_info.gender='female' then oldperson_info.gender end) as 'female' " \
+                        "FROM oldperson_info"
+    sql_numberOfNew_oldPeople = "SELECT COUNT(*) FROM oldperson_info WHERE DATE(oldperson_info.checkin_date)=CURDATE()"
+    sql_number_volunteer = "SELECT COUNT(*) FROM volunteer_info"
+    sql_numberOfActive_volunteer = "SELECT COUNT(*) FROM volunteer_info WHERE ISACTIVE='Yes'"
+    sql_number_emotion = "SELECT COUNT(case when event_info.event_desc='anger' then event_info.event_desc end) as 'anger', " \
+                         "COUNT(case when event_info.event_desc='disgust' then event_info.event_desc end) as 'disgust'," \
+                         "COUNT(case when event_info.event_desc='fear' then event_info.event_desc end) as 'fear'," \
+                         "COUNT(case when event_info.event_desc='happiness' then event_info.event_desc end) as 'happiness'," \
+                         "COUNT(case when event_info.event_desc='neutral' then event_info.event_desc end) as 'neutral'," \
+                         "COUNT(case when event_info.event_desc='sadness' then event_info.event_desc end) as 'anger'," \
+                         "COUNT(case when event_info.event_desc='surprise' then event_info.event_desc end) as 'surprise' " \
+                         "FROM event_info WHERE event_info.event_type=0 AND DATE(event_info.event_date)=CURDATE()"
+    sql_number_fall = "SELECT COUNT(case when DATE(event_info.event_date) BETWEEN DATE_SUB(CURDATE(),INTERVAL 3 DAY) AND CURDATE() then event_info.event_date end) as 'within_four_days', " \
+                      "SELECT COUNT(case when DATE(event_info.event_date) BETWEEN DATE_SUB(CURDATE(),INTERVAL 4 DAY) AND CURDATE() then event_info.event_date end) as 'within_five_days', " \
+                      "SELECT COUNT(case when DATE(event_info.event_date) BETWEEN DATE_SUB(CURDATE(),INTERVAL 5 DAY) AND CURDATE() then event_info.event_date end) as 'within_six_days', " \
+                      "SELECT COUNT(case when DATE(event_info.event_date) BETWEEN DATE_SUB(CURDATE(),INTERVAL 6 DAY) AND CURDATE() then event_info.event_date end) as 'within_seven_days', " \
+                      "SELECT COUNT(case when DATE(event_info.event_date) BETWEEN DATE_SUB(CURDATE(),INTERVAL 7 DAY) AND CURDATE() then event_info.event_date end) as 'within_eight_days', " \
+                      "SELECT COUNT(case when DATE(event_info.event_date) BETWEEN DATE_SUB(CURDATE(),INTERVAL 8 DAY) AND CURDATE() then event_info.event_date end) as 'within_nine_days', " \
+                      "SELECT COUNT(case when DATE(event_info.event_date) BETWEEN DATE_SUB(CURDATE(),INTERVAL 9 DAY) AND CURDATE() then event_info.event_date end) as 'within_ten_days', " \
+                      "SELECT COUNT(case when DATE(event_info.event_date) BETWEEN DATE_SUB(CURDATE(),INTERVAL 10 DAY) AND CURDATE() then event_info.event_date end) as 'within_eleven_days', "\
+                      "SELECT COUNT(case when DATE(event_info.event_date) BETWEEN DATE_SUB(CURDATE(),INTERVAL 11 DAY) AND CURDATE() then event_info.event_date end) as 'within_twelve_days', " \
+                      "SELECT COUNT(case when DATE(event_info.event_date) BETWEEN DATE_SUB(CURDATE(),INTERVAL 12 DAY) AND CURDATE() then event_info.event_date end) as 'within_thirteen_days', " \
+                      "FROM event_info WHERE event_info.event_type=3"
+    sql_number_intrusion = "SELECT COUNT(case when DATE(event_info.event_date) BETWEEN DATE_SUB(CURDATE(),INTERVAL 3 DAY) AND CURDATE() then event_info.event_date end) as 'within_four_days', " \
+                           "SELECT COUNT(case when DATE(event_info.event_date) BETWEEN DATE_SUB(CURDATE(),INTERVAL 4 DAY) AND CURDATE() then event_info.event_date end) as 'within_five_days', " \
+                           "SELECT COUNT(case when DATE(event_info.event_date) BETWEEN DATE_SUB(CURDATE(),INTERVAL 5 DAY) AND CURDATE() then event_info.event_date end) as 'within_six_days', " \
+                           "SELECT COUNT(case when DATE(event_info.event_date) BETWEEN DATE_SUB(CURDATE(),INTERVAL 6 DAY) AND CURDATE() then event_info.event_date end) as 'within_seven_days', " \
+                           "SELECT COUNT(case when DATE(event_info.event_date) BETWEEN DATE_SUB(CURDATE(),INTERVAL 7 DAY) AND CURDATE() then event_info.event_date end) as 'within_eight_days', " \
+                           "SELECT COUNT(case when DATE(event_info.event_date) BETWEEN DATE_SUB(CURDATE(),INTERVAL 8 DAY) AND CURDATE() then event_info.event_date end) as 'within_nine_days', " \
+                           "SELECT COUNT(case when DATE(event_info.event_date) BETWEEN DATE_SUB(CURDATE(),INTERVAL 9 DAY) AND CURDATE() then event_info.event_date end) as 'within_ten_days', " \
+                           "SELECT COUNT(case when DATE(event_info.event_date) BETWEEN DATE_SUB(CURDATE(),INTERVAL 10 DAY) AND CURDATE() then event_info.event_date end) as 'within_eleven_days', " \
+                           "SELECT COUNT(case when DATE(event_info.event_date) BETWEEN DATE_SUB(CURDATE(),INTERVAL 11 DAY) AND CURDATE() then event_info.event_date end) as 'within_twelve_days', " \
+                           "SELECT COUNT(case when DATE(event_info.event_date) BETWEEN DATE_SUB(CURDATE(),INTERVAL 12 DAY) AND CURDATE() then event_info.event_date end) as 'within_thirteen_days', " \
+                           "FROM event_info WHERE event_info.event_type=2 OR event_info.event_type=4"
+
+
+    number_oldPeople = db.session.execute(sql_number_oldPeople).all()
+    number_gender = db.session.execute(sql_number_gender).all()
+    numberOfNew_oldPeople = db.session.execute(sql_numberOfNew_oldPeople).all()
+    number_volunteer = db.session.execute(sql_number_volunteer).all()
+    numberOfActive_volunteer = db.session.execute(sql_numberOfActive_volunteer).all()
+    number_emotion = db.session.execute(sql_number_emotion).all()
+    emotion_dic = {'anger':number_emotion[0][0], 'disgust':number_emotion[0][1], 'fear':number_emotion[0][2], 'happiness':number_emotion[0][3],
+                 'neutral':number_emotion[0][4], 'sadness':number_emotion[0][5], 'surprise':number_emotion[0][6]}
+    number_fall = db.session.execute(sql_number_fall).all()
+    fall_dic = {'within_four_days':number_fall[0][0],'within_five_days':number_fall[0][1],'within_six_days':number_fall[0][2],
+                'within_seven_days':number_fall[0][3],'within_eight_days':number_fall[0][4],'within_nine_days':number_fall[0][5],
+                'within_ten_days':number_fall[0][6],'within_eleven_days':number_fall[0][7],'within_twelve_days':number_fall[0][8],
+                'within_thirteen_days':number_fall[0][9]}
+    number_intrusion = db.session.execute(sql_number_intrusion).all()
+    intrusion_dic = {'within_four_days':number_intrusion[0][0],'within_five_days':number_intrusion[0][1],'within_six_days':number_intrusion[0][2],
+                'within_seven_days':number_intrusion[0][3],'within_eight_days':number_intrusion[0][4],'within_nine_days':number_intrusion[0][5],
+                'within_ten_days':number_intrusion[0][6],'within_eleven_days':number_intrusion[0][7],'within_twelve_days':number_intrusion[0][8],
+                'within_thirteen_days':number_intrusion[0][9]}
+
+    result = {"number_oldPeople": number_oldPeople[0][0], "male_OldPeople": number_gender[0][0],
+              "female_OldPeople": number_gender[0][1],
+              "numberOfNew_oldPeople": numberOfNew_oldPeople[0][0], "number_volunteer": number_volunteer[0][0],
+              "numberOfActive_volunteer": numberOfActive_volunteer[0][0], 'emotion':emotion_dic,
+              "fall":fall_dic, "intrusion":intrusion_dic}
+    response = DataResponse(code=200, data=result, msg="Success get the data of home page!")
+    response = json.dumps(response.__dict__)
+
+    return response
+
+
 # 添加系统管理员请求体
 class AddSystemUserModel(BaseModel):
-    username:str
-    password:str
+    username: str
+    password: str
+
 
 # 系统管理员注册部分 即添加
 @app.route('/register', methods=['POST'])
-@siwa.doc(body=AddSystemUserModel,group="Test",tags="a")
+@siwa.doc(body=AddSystemUserModel, group="Test", tags="a")
 def add_sys_usr():
     body = request.json
-    sys_usr = SysUser(UserName=body['username'], Password=body['password'])
+    sys_usr = SysUser(UserName=body['username'], Password=body['password'], )
     db.session.add(sys_usr)
-    print ("add success")
+    print("add success")
 
-    response = BaseResponse(code=0, msg="Succeed to register")
+    response = BaseResponse(code=200, msg="Succeed to register")
     response = json.dumps(response.__dict__)
 
     return response
@@ -60,7 +132,7 @@ def add_sys_usr():
 
 # 系统管理员登录逻辑处理
 @app.route('/login', methods=['POST'])
-@siwa.doc(group="Test",tags="b")
+@siwa.doc(group="Test", tags="b")
 def login():
     body = request.json
     user_name = body['username']
@@ -81,8 +153,8 @@ def login():
 
 
 class tableOfOlderPersonModel(BaseModel):
-    result_dict:dict
-    result_list:list
+    result_dict: dict
+    result_list: list
 
 
 # 获取所有老人信息
@@ -99,7 +171,7 @@ def get_old_person_info():
                            'gender': result[3], 'room_num': result[4], 'checkin_date': str(result[5])}
             result_list.append(result_dict)
 
-    response = DataResponse(code=0, data=result_list, msg='Succeed to select all old persons!')
+    response = DataResponse(code=200, data=result_list, msg='Succeed to select all old persons!')
     response = json.dumps(response.__dict__)
 
     return response
@@ -111,11 +183,13 @@ def get_old_person_info():
 def add_old_person_info():
     body = request.json
     current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    old_person = OldPersonInfo(username=body['username'], gender=body['gender'], CREATED=current_time)
+    old_person = OldPersonInfo(username=body['username'], gender=body['gender'], phone=body['phone'],
+                               checkin_date=body['checkin_date'], checkout_date=body['checkout_date'],
+                               CREATED=current_time)
     db.session.add(old_person)
     db.session.commit()
 
-    response = BaseResponse(code=0, msg='Succeed to insert one old person!')
+    response = BaseResponse(code=200, msg='Succeed to insert one old person!')
     response = json.dumps(response.__dict__)
 
     return response
@@ -130,7 +204,7 @@ def delete_old_person():
     db.session.delete(old_person)
     db.session.commit()
 
-    response = BaseResponse(code=0, msg="Succeed to delete one old person!")
+    response = BaseResponse(code=200, msg="Succeed to delete one old person!")
     response = json.dumps(response.__dict__)
 
     return response
@@ -145,13 +219,13 @@ def update_old_person():
     OldPersonInfo.query.filter_by(ID=body['id']).update({'username': body['userName'], 'UPDATED': current_time})
     db.session.commit()
 
-    response = BaseResponse(code=0, msg="Succeed to update old person!")
+    response = BaseResponse(code=200, msg="Succeed to update old person!")
     response = json.dumps(response.__dict__)
 
     return response
 
 
-# 获取所有志愿者信息
+# 获取所有义工信息
 @app.route('/table_volunteer', methods=['GET'])
 @siwa.doc()
 def get_volunteer():
@@ -165,23 +239,25 @@ def get_volunteer():
                            'gender': result[3], 'phone': result[4], 'is_active': str(result[5])}
         result_list.append(result_dict)
 
-    response = DataResponse(code=0, data=result_list, msg='Succeed to select all old persons!')
+    response = DataResponse(code=200, data=result_list, msg='Succeed to select all old persons!')
     response = json.dumps(response.__dict__)
 
     return response
 
 
-# 添加一个新的志愿者
+# 添加一个新的义工
 @app.route('/addVolunteer', methods=['POST'])
 @siwa.doc()
 def add_volunteer():
     body = request.json
     current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    volunteer = VolunteerInfo(name=body['name'], gender=['gender'], CREATED=current_time)
+    volunteer = VolunteerInfo(name=body['name'], gender=['gender'], phone=body['phone'],
+                              checkin_date=body['checkin_date'], checkout_date=body['checkout_date'],
+                              ISACTIVE=body['isActive'], CREATED=current_time)
     db.session.add(volunteer)
     db.session.commit()
 
-    response = BaseResponse(code=0, msg="Succeed to insert one volunteer!")
+    response = BaseResponse(code=200, msg="Succeed to insert one volunteer!")
     response = json.dumps(response.__dict__)
 
     return response
@@ -196,10 +272,95 @@ def get_events():
     for result in EventInfo.query.filter_by(event_type=body['eventType']).all():
         result_list.append(result.to_json())
 
-    response = DataResponse(code=0, data=result_list, msg='Succeed to get specific events!')
+    response = DataResponse(code=200, data=result_list, msg='Succeed to get specific events!')
     response = json.dumps(response.__dict__)
 
     return response
+
+
+# 老人情感检测数据
+@app.route('/table_facial_expression', methods=['GET'])
+def get_facial_expression():
+    result_list = []
+
+    sql = "SELECT oldperson_id,username, event_location, event_date, event_desc, event_image_dir FROM oldPerson_info, event_info WHERE event_info.oldperson_id = oldPerson_info.ID AND event_type=0"
+
+    for result in db.session.execute(sql):
+        with open(result[5], 'rb') as f:
+            data = f.read()
+            encode_str = "data:image/jpg;base64," + str(base64.b64encode(data), 'utf-8')
+            result_dict = {'oldperson_id':result[0],'userName': result[1], 'eventLocation': result[2], 'eventDate': str(result[3]),
+                           'eventDesc': result[4], 'eventPhoto': encode_str}
+            result_list.append(result_dict)
+
+    response = DataResponse(code=200, data=result_list, msg='Succeed to get facial expression events!')
+    response = json.dumps(response.__dict__)
+
+    return response
+
+
+# 获取区域入侵检测异常数据
+@app.route('/table_instrusion', methods=['GET'])
+def get_intrusion():
+    result_list = []
+
+    sql = "SELECT event_location, event_date, event_image_dir FROM event_info WHERE event_type=4"
+
+    for result in db.session.execute(sql):
+        with open(result[2], 'rb') as f:
+            data = f.read()
+            encode_str = "data:image/jpg;base64," + str(base64.b64encode(data), 'utf-8')
+            result_dict = {'eventLocation': result[0], 'eventDate': str(result[1]), 'eventPhoto': encode_str}
+            result_list.append(result_dict)
+
+    response = DataResponse(code=200, data=result_list, msg='Succeed to get intrusion events!')
+    response = json.dumps(response.__dict__)
+
+    return response
+
+
+# 获取老人摔倒检测数据
+@app.route('/table_fall', methods=['GET'])
+def get_fall():
+    result_list = []
+
+    sql = "SELECT event_location, event_date, event_image_dir FROM event_info WHERE event_type=3"
+
+    for result in db.session.execute(sql):
+        with open(result[2], 'rb') as f:
+            data = f.read()
+            encode_str = "data:image/jpg;base64," + str(base64.b64encode(data), 'utf-8')
+            result_dict = {'eventLocation': result[0], 'eventDate': str(result[1]), 'eventPhoto': encode_str}
+            result_list.append(result_dict)
+
+    response = DataResponse(code=200, data=result_list, msg='Succeed to get fall events!')
+    response = json.dumps(response.__dict__)
+
+    return response
+
+
+# 获取老人与护工交互检测异常数据
+@app.route('/table_interaction', methods=['GET'])
+def get_interaction():
+    result_list = []
+
+    sql = "SELECT oldperson_id, username, volunteer_id, `name`, event_location, event_date, event_image_dir FROM event_info, volunteer_info, oldPerson_info " \
+          "WHERE event_type=1 AND event_info.oldperson_id = oldPerson_info.ID AND event_info.volunteer_id = volunteer_info.id"
+
+    for result in db.session.execute(sql):
+        with open(result[6], 'rb') as f:
+            data = f.read()
+            encode_str = "data:image/jpg;base64," + str(base64.b64encode(data), 'utf-8')
+            result_dict = {'oldPersonId': result[0], 'oldPersonName': result[1], 'volunteerId': result[2],
+                           'volunteerName': result[3],
+                           'eventLocation': result[4], 'eventDate': str(result[5]), 'eventPhoto': encode_str}
+            result_list.append(result_dict)
+
+    response = DataResponse(code=200, data=result_list, msg='Succeed to get interaction events!')
+    response = json.dumps(response.__dict__)
+
+    return response
+
 
 # 添加一条事件记录
 @app.route('/addEvent', methods=['POST'])
@@ -234,13 +395,12 @@ def add_event():
     db.session.add(event)
     db.session.commit()
 
-    response = BaseResponse(code=0, msg='Succeed to add event')
+    response = BaseResponse(code=200, msg='Succeed to add event')
     response = json.dumps(response.__dict__)
 
     return response
 
 
-
-
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port='9656')
+    # get_homePage_Data()
